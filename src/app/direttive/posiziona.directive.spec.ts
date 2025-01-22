be3ba9c830de54109f202b53c940ac43da57225e
Marta <gcf.marta@gmail.com>
2025-01-22 18:16:49 +0100

    aggiunta leaderboard + posiziona elementi

---
+++ src/app/direttive/posiziona.directive.spec.ts
@@ -0,0 +1,8 @@
+// import { PosizionaDirective } from './posiziona.directive';
+//
+// describe('PosizionaDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new PosizionaDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
