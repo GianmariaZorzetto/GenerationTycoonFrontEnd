07d2c155cdfaa92d1baa78f9740e7c720844275c
Riccardo <panettariccardo97@gmail.com>
2025-01-23 09:48:43 +0100

    Schermata registrazione (da fixare) + incipit Model (da vedere meglio)

---
+++ src/app/Component/schermata-benvenuto/schermata-benvenuto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schermata-benvenuto',
+  imports: [],
+  templateUrl: './schermata-benvenuto.component.html',
+  styleUrl: './schermata-benvenuto.component.css'
+})
+export class SchermataBenvenutoComponent {
+
+}
