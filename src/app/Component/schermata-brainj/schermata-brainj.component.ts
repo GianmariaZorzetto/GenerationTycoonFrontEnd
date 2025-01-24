4ff8ff926b33352f3c592c7015295ffe4ab092c9
DubStray <francesco.gaming00@gmail.com>
2025-01-24 14:50:06 +0100

    Fix grafici su BrainJ

--- src/app/Component/schermata-brainj/schermata-brainj.component.ts
+++ src/app/Component/schermata-brainj/schermata-brainj.component.ts
@@ -16,6 +16,6 @@ import {RouterLink} from '@angular/router';
 })
 export class SchermataBrainjComponent {
   constructor(private bg: BackgroundService) {
-    this.bg.changeBackground("brainj/brainj_finale.png")
+    this.bg.changeBackground("brainj/brainj_theme_final.png")
   }
 }
