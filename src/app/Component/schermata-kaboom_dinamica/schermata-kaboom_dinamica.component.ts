9df918b05c55f2cef3d10dae368fee9077d60d2e
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-01-25 17:00:33 +0100

    implementati kaboom

--- src/app/Component/schermata-kaboom_dinamica/schermata-kaboom_dinamica.component.ts
+++ src/app/Component/schermata-kaboom_dinamica/schermata-kaboom_dinamica.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {BackgroundService} from '../../../../services/background.service';
-import {NgIf} from '@angular/common';
 import {PosizionaDirective} from '../../direttive/posiziona.directive';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {KaboomDTOResp} from '../../model/KaboomDTOResp';
+import {HttpService} from '../../services/http-service.service';
+import {UserScoreDTOReq} from '../../model/UserScoreDTOReq';
 
 @Component({
   selector: 'app-schermata-kaboom',
   imports: [
-    NgIf,
     PosizionaDirective,
     RouterLink
   ],
@@ -15,11 +16,57 @@ import {RouterLink} from '@angular/router';
   standalone: true,
   styleUrl: './schermata-kaboom_dinamica.component.css'
 })
-export class SchermataKaboom_dinamicaComponent {
+export class SchermataKaboom_dinamicaComponent implements OnDestroy {
 
-  constructor(private bg:BackgroundService)
-  {
+  kaboom: KaboomDTOResp
+
+  startDate: Date
+
+  solution: string
+
+  quizResult: { result: boolean, score: number }
+
+  constructor(private bg: BackgroundService, private route: Router, private httpService: HttpService) {
     this.bg.changeBackground("kaboom/KaboomSfondoFinale.png")
+    this.kaboom = this.httpService.getSingleKaboom()
+    console.log(this.kaboom)
+    this.startDate = new Date()
+    this.solution = ""
+    this.quizResult = {result: false, score: 0}
   }
 
+  ngOnDestroy(): void {
+    console.log("SONO IN ONDESTROY")
+    if (!this.quizResult.result) this.httpService.loseLife()
+    this.httpService.quizCompleted()
+    this.httpService.quizResult = this.quizResult
+    this.httpService.score += this.quizResult.score
+  }
+
+  checkAnswer(color: string) {
+    if (color === this.kaboom.correctColor) {
+      if (this.httpService.life == 0) this.quizResult = {result: true, score: 0}
+      let endDate = new Date()
+      let dto: UserScoreDTOReq = {
+        difficulty: this.httpService.userLoginReqDto.difficulty,
+        endTime: endDate,
+        hp: this.httpService.life,
+        startTime: this.startDate
+      }
+      this.httpService.calculateScore(dto).subscribe({
+        next: (res) => {
+          this.quizResult = {result: true, score: res.score}
+          this.route.navigate(["/kaboom_dinamico_risultato"])
+        },
+        error: (err) => {
+          alert("Errore nel calcolo del punteggio, annullamento del quiz.")
+          this.quizResult = {result: true, score: 0}
+          this.httpService.numberOfQuiz += 1
+          this.route.navigate(["/kaboom_dinamico_risultato"])
+        }
+      })
+    } else {
+      this.route.navigate(["/kaboom_dinamico_risultato"])
+    }
+  }
 }
