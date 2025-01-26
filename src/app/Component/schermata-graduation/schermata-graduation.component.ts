d8e0d1df728f78d91ec7e2a07e6922e854618102
Marta <gcf.marta@gmail.com>
2025-01-26 17:57:28 +0100

    sistemata schermata graduation

--- src/app/Component/schermata-graduation/schermata-graduation.component.ts
+++ src/app/Component/schermata-graduation/schermata-graduation.component.ts
@@ -2,10 +2,13 @@ import {Component} from '@angular/core';
 import {BackgroundService} from '../../../../services/background.service';
 import {HttpService} from '../../services/http-service.service';
 import {UserLeaderboardDTOResp} from '../../model/UserLeaderboardDTOResp';
+import {PosizionaDirective} from '../../direttive/posiziona.directive';
 
 @Component({
   selector: 'app-schermata-graduation',
-  imports: [],
+  imports: [
+    PosizionaDirective
+  ],
   templateUrl: './schermata-graduation.component.html',
   standalone: true,
   styleUrl: './schermata-graduation.component.css'
